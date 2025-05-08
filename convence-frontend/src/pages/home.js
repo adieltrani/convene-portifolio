84482d00ebc817d86821f5290ace40d144592127
adiexxoff <90799445+adiexxoff@users.noreply.github.com>
2025-05-08 19:41:45 -0300

    front-end

---
+++ convence-frontend/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () =>{
+  return(
+    <div>
+        <h1>Bem Vindo ao Convence!</h1>
+        <p>Encronte os melhores eventos e participe!</p>
+    </div>
+  
+  );  
+}
+
+export default Home;

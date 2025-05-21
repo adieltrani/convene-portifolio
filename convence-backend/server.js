7616e5cbd636b5761260f4eb5ee8f18591dbad92
Adiel Trani <117945288+adiexx@users.noreply.github.com>
2025-05-21 14:23:56 -0300

    clean-code
    Commenting on the code to improve understanding and relocating some items to make it more visually pleasing

--- convence-backend/server.js
+++ convence-backend/server.js
@@ -4,56 +4,56 @@ import mongoose from 'mongoose';
 import ingol from './apis/ingol.js';
 import cors from 'cors';
 
-dotenv.config();
+const app = express(); //Cria Uma Instancia do app em express
+const PORT = 3000; //Define a porta que o Server vai rodar
 
-const app = express();
-const PORT = 3000;
-
-app.use(cors()); 
-app.use(express.json());
+dotenv.config(); //Premite o uso de variaveis de ambiente
+app.use(cors()); //Premite a comunicacao com o Front em outra porta
+app.use(express.json()); //Permite o middleware para persear requisicoes JSON
 
 const connectDB = async () => {
   try{
      await mongoose.connect(process.env.MONGO_URI);
-     console.log('Conectou kraiokkk');
+     console.log('Banco de Dados Conectado!');
 
   }catch(error){
-    console.log('moio paizao', error)
+    console.log('Erro ao se conectar ao Bando de Dados!', error)
 
   }
 
-}
-connectDB();
+} //Funcao que cria uma conexao com o DB
+
+connectDB(); //Inicia a funcao que inicia a conexao com o DB
 
 app.post("/register", async (req, res) => {
    
  try {
-   const {email} = req.body; //pega o email na pagina longin via req.body
-   const usuarioexist = await ingol.findOne({email: email}); //procura no db um dado tipo email definido na tabela q tenha o email(dado)
-
+   const {email} = req.body;
+   const usuarioexist = await ingol.findOne({email: email});
+   //Aqui verifica se esse email existe no DB
    if (usuarioexist){
       console.log('Ja existe uma conta com esse email!')
       return res.status(409).json({mensagem: "Ja existe uma conta com esse email!"})
+      //Aqui retorna pro usuario se existe
    }
    else{
       const novoLogin = await ingol.create(req.body)
       res.json({mensagem: "Deu certo!"})
+      //Aqui cria um novo usuario caso nao existir
    }
 
  } catch (error) {
     console.error("Erro ao criar login:", error);
     res.status(500).json({ error: error.message });
- }
-});
+ }}); 
 
-app.get("/login", async (req, res) => {
- try {
-    const ingols = await ingol.find();
-    res.json(ingols)
- } catch (error) {
-    console.error("Erro ao criar login:", error);
-    res.status(500).json({ error: error.message });
- }
-});
+// app.get("/login", async (req, res) => {
+//  try {
+//     const ingols = await ingol.find();
+//     res.json(ingols)
+//  } catch (error) {
+//     console.error("Erro ao criar login:", error);
+//     res.status(500).json({ error: error.message });
+//  }});
 
 app.listen(PORT, () => console.log(`O Servidor está rodando na porta ${PORT}`));

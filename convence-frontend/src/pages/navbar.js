c411efe12f37776c3135a5581b369ddb1e3de11f
adieltrani <adieltranixd@gmail.com>
2025-06-01 18:34:03 -0300

    front-end-update

--- convence-frontend/src/pages/navbar.js
+++ convence-frontend/src/pages/navbar.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 function Navbar(){
   return (
     <header className='navbar'>
+      <div> <img src="/logo.png" alt="Katherine Johnson" className='logo-nav'/></div>
       <div className='menu'>
         <h3>Convence</h3>
         <nav className='item-menu'>
-          <Link to="/"><b>Home</b></Link>
-          <Link to="/eventos"><b>Eventos</b></Link>
-          <Link to="/ingresos"><b>Ingresos</b></Link>
-          <Link to="/login"><b>Login</b></Link>
+          <Link to="/"  className="nav-link"><b>Home</b></Link>
+          <Link to="/eventos"  className="nav-link"><b>Eventos</b></Link>
+          <Link to="/ingresos"  className="nav-link"><b>Ingresos</b></Link>
+          <Link to="/login"  className="nav-link"><b>Login</b></Link>
         </nav>
       </div>
     </header>
